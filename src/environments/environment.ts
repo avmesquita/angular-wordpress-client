d28d1166a76633d958d93aa0599515ad77f9ac68
Andre Mesquita <avmesquita@gmail.com>
2022-02-23 14:17:22 +0000

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  wordpressUrl: 'https://impeto.com.br/wp-json/wp/v2/',
+  wordpressUrl: 'http://localhost/wp-json/wp/v2/',
   wpUsers: 'users/',
   wpPosts: 'posts/',
   wpCategories: 'categories/',
